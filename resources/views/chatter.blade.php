62bacf9bda0758117cfa6c4c26c9604b763182c7
darkheart101 <kurokokoro@windowslive.com>
2019-07-04 20:37:46 +0300

    init commit

---
+++ resources/views/chatter.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Pusher Test</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
+        <script>
+
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+
+            var pusher = new Pusher('8a616bf0e905afe416d1', {
+                cluster: 'eu',
+                forceTLS: true
+            });
+
+            var channel = pusher.subscribe('my-channel');
+            channel.bind('form-submited', function(data) {
+                alert(JSON.stringify(data));
+            });
+        </script>
+    </head>
+
+    <body>
+
+        <h1>Pusher Test</h1>
+        <p>
+            Try publishing an event to channel <code>my-channel</code>
+            with event name <code>my-event</code>.
+        </p>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+use App\Events\FormSubmited;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('chatter');
+});
+
+Route::get('/sender', function () {
+    return view('sender');
+});
+
+Route::post('/sender', function () {
+    $text = request()->text;
+    event(new FormSubmited($text));
+});
