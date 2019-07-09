69590e4c2ed65e8d0cb75b3506e12b3b30c0c1ab
darkheart101 <kurokokoro@windowslive.com>
2019-07-09 20:57:37 +0300

    save message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Events\FormSubmited;
+use Illuminate\Support\Facades\Auth;
+use App\Message;
 
 class MessageController extends Controller
 {
     public function send(Request $request)
     {
         $message = request()->message;
+        $user_id = Auth::id();
+
+        Message::create(['message'=>$message, 'user_id'=>$user_id]);
+
         event(new FormSubmited($message));
     }
 
