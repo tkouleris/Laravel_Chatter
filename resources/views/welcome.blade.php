b1d80a64fee4f2eef815b4e478e260b3166e4ad5
darkheart101 <kurokokoro@windowslive.com>
2019-07-11 20:54:30 +0300

    route fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Chatter
                 </div>
 
                 <div class="links">
