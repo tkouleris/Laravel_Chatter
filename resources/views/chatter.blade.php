4bcb8b443db2ac8f12fdea35210305c7dd8b7d30
darkheart101 <kurokokoro@windowslive.com>
2019-07-21 08:57:00 +0300

    cleanup

--- resources/views/chatter.blade.php
+++ resources/views/chatter.blade.php
@@ -1,15 +1,3 @@
-{{-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://js.pusher.com/4.4/pusher.min.js"></script> --}}
-<!------ Include the above in your HEAD tag ---------->
-
-{{-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script> --}}
-<!------ Include the above in your HEAD tag ---------->
-
-
 <!DOCTYPE html><html class=''>
 <head>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
